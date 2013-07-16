b4096aca6845a1ba8a13c085b70d1ebe3f41a32d
Jim Dowling <jdowling@kth.se>
2013-07-16 07:47:35 +0200

    Removed hp-server-main, superceded by nattraveser-test

--- hole-punching/hp-server-main/kajsa.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mvn exec:java -Dexec.mainClass=se.sics.kompics.nat.hp.servermain.HPServerMain -Dexec.args="lucan.sics.se 0"
--- hole-punching/hp-server-main/lucan.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mvn exec:java -Dexec.mainClass=se.sics.kompics.nat.hp.servermain.HPServerMain -Dexec.args="kajsa.sics.se 0"
