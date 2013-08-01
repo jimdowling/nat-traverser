3c22e1687a8b2a3152a28d58da25a6c7d63c7585
Steffen <steffeng@kth.se>
2013-08-01 09:57:32 +0200

    proper connection shutdown

--- network/netty/src/test/java/se/sics/kompics/SetsExchangeTest.java
+++ network/netty/src/test/java/se/sics/kompics/SetsExchangeTest.java
@@ -3,7 +3,9 @@ package se.sics.kompics;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.Semaphore;
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -24,8 +26,7 @@ import se.sics.gvod.net.NatNetworkControl;
 import se.sics.gvod.net.NettyInit;
 import se.sics.gvod.net.NettyNetwork;
 import se.sics.gvod.net.Transport;
-import se.sics.gvod.net.events.PortBindRequest;
-import se.sics.gvod.net.events.PortBindResponse;
+import se.sics.gvod.net.events.*;
 import se.sics.gvod.timer.ScheduleTimeout;
 import se.sics.gvod.timer.Timer;
 import se.sics.gvod.timer.java.JavaTimer;
@@ -78,6 +79,18 @@ public class SetsExchangeTest extends TestCase {
         private VodDescriptor nodeDesc;
         private List<VodDescriptor> nodes;
 
+        private class ClientPortDeleteResponse extends PortDeleteResponse {
+            public ClientPortDeleteResponse(PortDeleteRequest request, Object key) {
+                super(request, key);
+            }
+        }
+
+        private class ServerPortDeleteResponse extends PortDeleteResponse {
+            public ServerPortDeleteResponse(PortDeleteRequest request, Object key) {
+                super(request, key);
+            }
+        }
+
         public TestStClientComponent() {
             timer = create(JavaTimer.class);
             client = create(NettyNetwork.class);
@@ -111,39 +124,31 @@ public class SetsExchangeTest extends TestCase {
             subscribe(handleSetsExchangeResponse, server.getPositive(VodNetwork.class));
             subscribe(handleTestPortBindResponse, client.getPositive(NatNetworkControl.class));
             subscribe(handleTestPortBindResponse, server.getPositive(NatNetworkControl.class));
+            subscribe(handleClientPortDeletionResponse, client.getPositive(NatNetworkControl.class));
+            subscribe(handleServerPortDeletionResponse, server.getPositive(NatNetworkControl.class));
 
             trigger(new NettyInit(132, true,
                     BaseMsgFrameDecoder.class), client.getControl());
             trigger(new NettyInit(132, true,
                     BaseMsgFrameDecoder.class), server.getControl());
-
-            PortBindRequest pb1 = new PortBindRequest(clientAddr.getPeerAddress(),
-                    Transport.UDP);
-            PortBindResponse pbr1 = new TestPortBindResponse(pb1);
-            trigger(pb1, client.getPositive(NatNetworkControl.class));
-
-            PortBindRequest pb2 = new PortBindRequest(serverAddr.getPeerAddress(), Transport.UDP);
-            PortBindResponse pbr2 = new TestPortBindResponse(pb2);
-            trigger(pb2, server.getPositive(NatNetworkControl.class));
-
-
         }
         public Handler<Start> handleStart = new Handler<Start>() {
             public void handle(Start event) {
                 System.out.println("Starting");
-                ScheduleTimeout st = new ScheduleTimeout(10 * 1000);
-                SetsExchangeMsg.RequestTimeout mt = new SetsExchangeMsg.RequestTimeout(st,
-                        serverAddr);
-                st.setTimeoutEvent(mt);
-                trigger(new SetsExchangeMsg.Request(clientAddr, serverAddr, clientAddr.getId(),
-                        serverAddr.getId(), mt.getTimeoutId()),
-                        client.getPositive(VodNetwork.class));
-                trigger(st, timer.getPositive(Timer.class));
+                PortBindRequest pb1 = new PortBindRequest(clientAddr.getPeerAddress(),
+                        Transport.UDP);
+                PortBindResponse pbr1 = new TestPortBindResponse(pb1);
+                trigger(pb1, client.getPositive(NatNetworkControl.class));
+
+                PortBindRequest pb2 = new PortBindRequest(serverAddr.getPeerAddress(), Transport.UDP);
+                PortBindResponse pbr2 = new TestPortBindResponse(pb2);
+                trigger(pb2, server.getPositive(NatNetworkControl.class));
             }
         };
         public Handler<SetsExchangeMsg.Request> handleSetsExchangeRequest = new Handler<SetsExchangeMsg.Request>() {
             @Override
             public void handle(SetsExchangeMsg.Request event) {
+                logger.info("Received SetExchangeRequest");
                 trigger(new SetsExchangeMsg.Response(serverAddr.getNodeAddress(), event, nodes, nodes),
                         server.getPositive(VodNetwork.class));
             }
@@ -153,17 +158,51 @@ public class SetsExchangeTest extends TestCase {
                 = new Handler<TestPortBindResponse>() {
             @Override
             public void handle(TestPortBindResponse event) {
+                logger.info("Received PortBindResponse");
                 if (event.getStatus() != TestPortBindResponse.Status.SUCCESS) {
                     testObj.fail(true);
+                } else {
+                    ScheduleTimeout st = new ScheduleTimeout(10 * 1000);
+                    SetsExchangeMsg.RequestTimeout mt = new SetsExchangeMsg.RequestTimeout(st,
+                            serverAddr);
+                    st.setTimeoutEvent(mt);
+                    trigger(new SetsExchangeMsg.Request(clientAddr, serverAddr, clientAddr.getId(),
+                            serverAddr.getId(), mt.getTimeoutId()),
+                            client.getPositive(VodNetwork.class));
+                    trigger(st, timer.getPositive(Timer.class));
                 }
             }
         };
         public Handler<SetsExchangeMsg.Response> handleSetsExchangeResponse = new Handler<SetsExchangeMsg.Response>() {
             @Override
             public void handle(SetsExchangeMsg.Response event) {
+                logger.info("Received SetExchangeResponse");
+                Set set = new HashSet<Integer>();
+                set.add(clientAddr.getPort());
+                PortDeleteRequest request = new PortDeleteRequest(0, set, Transport.UDP);
+                request.setResponse(new ClientPortDeleteResponse(request, 0) {
+                });
+                trigger(request, client.getPositive(NatNetworkControl.class));
+            }
+        };
+        public Handler<ClientPortDeleteResponse> handleClientPortDeletionResponse = new Handler<ClientPortDeleteResponse>() {
+            @Override
+            public void handle(ClientPortDeleteResponse event) {
+                logger.info("Received PortDeleteResponse for client");
+                Set set = new HashSet<Integer>();
+                set.add(clientAddr.getPort());
+                PortDeleteRequest request = new PortDeleteRequest(0, set, Transport.UDP);
+                request.setResponse(new ServerPortDeleteResponse(request, 0) {
+                });
+                trigger(request, server.getPositive(NatNetworkControl.class));
+            }
+        };
+        public Handler<ServerPortDeleteResponse> handleServerPortDeletionResponse = new Handler<ServerPortDeleteResponse>() {
+            @Override
+            public void handle(ServerPortDeleteResponse event) {
+                logger.info("Received PortDeleteResponse for server");
                 trigger(new Stop(), client.getControl());
                 trigger(new Stop(), server.getControl());
-                System.out.println("Data Response");
                 testObj.pass();
             }
         };
