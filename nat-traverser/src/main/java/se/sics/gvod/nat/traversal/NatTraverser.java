7a79bc060e79afb096ad357cf43cc0a61f636382
Jim Dowling <jdowling@kth.se>
2013-09-12 11:02:55 +0200

    Bug in caching nat type - still to fix

--- nat-traverser/src/main/java/se/sics/gvod/nat/traversal/NatTraverser.java
+++ nat-traverser/src/main/java/se/sics/gvod/nat/traversal/NatTraverser.java
@@ -1034,7 +1034,7 @@ public class NatTraverser extends MsgRetryComponent {
             delegator.doTrigger(new Join(bootstrappers),
                     parentMaker.getPositive(ParentMakerPort.class));
         }
-        cacheStunResults();
+//        cacheStunResults();
     }
     private Handler<GetNatTypeResponse> handleGetNatTypeResponse = new Handler<GetNatTypeResponse>() {
         @Override
